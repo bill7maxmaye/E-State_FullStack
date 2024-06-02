b90116bf1c0e80719b336111205d425ced61aef5
bill7maxmaye <bilenmehalek7@gmail.com>
2024-06-02 23:35:55 +0300

    commit

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,11 +1,15 @@
 import bcrypt from "bcrypt";
-
+import { prisma } from "../lib/prisma.js";
 export const register = async (req, res) => {
   const { name, email, password } = req.body;
 
   //hash password
   const hashedpassword = await bcrypt.hash(password, 10);
-  console.log(hashedpassword);
+  const newUser = await prisma.user.create({
+    data: { name, email, hashedpassword },
+  });
+
+  console.log(newUser);
 };
 
 export const login = (req, res) => {
