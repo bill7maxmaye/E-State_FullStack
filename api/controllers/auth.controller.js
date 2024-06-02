c4eaedf07718b1d5a5bfcb5d1ecf341b6869c34f
bill7maxmaye <bilenmehalek7@gmail.com>
2024-06-03 01:07:50 +0300

    commit

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -4,25 +4,75 @@ import prisma from "../lib/prisma.js";
 export const register = async (req, res) => {
   const { username, email, password } = req.body;
 
-  // HASH THE PASSWORD
-  const hashedPassword = await bcrypt.hash(password, 10);
+  try {
+    // HASH THE PASSWORD
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-  console.log(hashedPassword);
+    console.log(hashedPassword);
 
-  // CREATE A NEW USER AND SAVE TO DB
-  const newUser = await prisma.user.create({
-    data: {
-      username,
-      email,
-      password: hashedPassword,
-    },
-  });
+    // CREATE A NEW USER AND SAVE TO DB
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password: hashedPassword,
+      },
+    });
 
-  console.log(newUser);
+    console.log(newUser);
+    res.status(201).json({ message: "User created successfuly" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Failed to create a user" });
+  }
 };
 
-export const login = (req, res) => {
-  console.log("login endpoint called");
+export const login = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    //CHECK IF USER EXISTS
+    const user = await prisma.user.findUnique({
+      where: { username },
+    });
+
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    //CHECK IF PASSWORD IS CORRECT
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(400).json({ message: "Invlaid Credentials" });
+    }
+    //GENERATE COOKIE TOKEN AND SEND TO THE USER
+
+    const age = 1000 * 60 * 60 * 24 * 7;
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+        isAdmin: false,
+      },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: age }
+    );
+    //This part extracts the password property from the user object and assigns it to a new variable called userPassword
+    //const { ...userInfo } = user; This part takes the remaining properties of the user object (excluding password) and collects them into a new object called userInfo.
+    const { password: userPassword, ...userInfo } = user;
+
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+        // secure:true,
+        maxAge: age,
+      })
+      .status(200)
+      .json(userInfo);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Failed to login" });
+  }
 };
 
 export const logout = (req, res) => {
